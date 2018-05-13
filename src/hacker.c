e1f83b797f453b27fdc4b157a32682b09ea46ec3
Joao Barbirato <joaobarbirato@gmail.com>
2018-05-12 23:07:51 -0300

    Criada funcao dica do livro

--- src/hacker.c
+++ src/hacker.c
@@ -26,4 +26,36 @@ if isCaptain:
     mutex.signal()                      # captain releases the mutex
 
 */
-#include "../headers/hacker.h"
+#include "../headers/main.h"
+#include "../headers/boat.h"
+
+int manageHackersBoat(Boat * b, int * is_captain){
+    pthread_mutex_lock(b->mutex);
+    b->n_hackers++;
+    if(b->n_hackers == 4){
+        sem_init(b->hq->sem, 0, 4);
+        b->n_hackers = 0;
+        *is_captain = 1;
+    }else if ((b->n_hackers && b->n_serfs) == 2){
+        sem_init(b->hq->sem, 0, 2);
+        sem_init(b->sq->sem, 0, 2);
+        b->n_hackers = 0;
+        b->n_serfs -= 2;
+        *is_captain = 1;
+    }else
+        pthread_mutex_unlock(b->mutex);
+    
+    sem_wait(b->hq->sem);
+
+/*
+    //TODO:
+    board()
+    barrier.wait()
+
+    if isCaptain:
+        rowBoat()
+        mutex.signal()                      # captain releases the mutex
+*/
+
+    return 0; //?
+};
