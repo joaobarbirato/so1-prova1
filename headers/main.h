35020191876306decee59ca38d8939c0034554a9
Joao Barbirato <joaobarbirato@gmail.com>
2018-05-12 23:06:28 -0300

    Reestruturado 'main.h'

--- headers/main.h
+++ headers/main.h
@@ -1,4 +1,12 @@
+#pragma once
 // default libs
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <time.h>
+#include <semaphore.h>
+
+// custom libs
+#include "queue.h"
+#include "hacker.h"
+#include "serf.h"
