0ad45ab1ddb7591187eb6d1579f1a7d31f9f3973
Joao Barbirato <joaobarbirato@gmail.com>
2018-05-12 23:08:22 -0300

    Adicionados operadores das filas

--- src/queue.c
+++ src/queue.c
@@ -1,1 +1,19 @@
 #include "../headers/queue.h"
+
+// Insert
+int insert_queue(Queue*q, const int x){
+    int i;
+    for(i = 0; i < q->size; i++){
+        if(q->queue[i] == -1)
+            q->queue[i] = x;
+            return i;
+    }
+    return -1;
+}; // end insert
+
+// Remove
+int remove_queue(Queue*q, const int pos){
+    int r = q->queue[pos];
+    q->queue[pos] = -1;
+    return r;
+} // end remove
