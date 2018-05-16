57289aee700a3d7bff92e5d8f7fac2b07a9912c9
Joao Barbirato <joaobarbirato@gmail.com>
2018-05-15 23:38:32 -0300

    Adicionadas inicializacoes principais

--- script/main.c
+++ script/main.c
@@ -4,11 +4,23 @@
 
 int main(){
     srand(time(NULL)); // random seed
-
+    int is_captain; // false
     Boat boat;
+    boat.cap = 4;
+    Queue q;
 
+    // a partir daqui, se repete
+    is_captain = 0;
     boat.n_hackers = 0;
     boat.n_serfs = 0;
+    pthread_barrier_init(&(boat.barrier), NULL, 4);
+    pthread_mutex_init(&(boat.mutex), NULL);
+    pthread_mutex_lock(&(boat.mutex));
+
+
+
+
+
 
     printf("Helo world!\n");
     return 0;
