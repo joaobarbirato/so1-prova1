edfe553701ea5c1b8c9a77263ade21fc88279ff7
Gabrieli Santos <gabrielisantos17.gs@gmail.com>
2018-05-15 21:38:29 -0300

    entendendo codigo

--- headers/boat.h
+++ headers/boat.h
@@ -2,9 +2,9 @@
 #include "main.h"
 
 typedef struct{
-    HackerQueue * hq;
-    SerfQueue * sq;
-    pthread_mutex_t * mutex;
+    HackerQueue * hq; //fila de hackers
+    SerfQueue * sq; //fila de funcionarios
+    pthread_mutex_t * mutex; 
     int n_hackers;   // number of hackers at the boat
     int n_serfs;
 } Boat;
