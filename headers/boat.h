6a68dc8198955cd76199e29cdacbabc640e9478a
Joao Barbirato <joaobarbirato@gmail.com>
2018-05-17 16:56:21 -0300

    Adicionados prototipos de funcoes

--- headers/boat.h
+++ headers/boat.h
@@ -1,12 +1,18 @@
 #pragma once
 #include "main.h"
 
+// Boat definition
 typedef struct{
-    pthread_mutex_t mutex; 
-    pthread_barrier_t barrier;
+    pthread_mutex_t mutex;      // mutex
+    pthread_barrier_t barrier;  // barrier
     int n_hackers;   // number of hackers at the boat
-    int n_serfs;
-    int cap;
+    int n_serfs;     // number of serfs at the boat
+    int cap;         // person's cap
+    sem_t hackers_queue;    // not used, why?
+    sem_t serfs_queue;      // not used, why?
 } Boat;
 
+// prototypes
 void * board(void *a);
+void row_boat(sem_t * sem_queue, int * is_captain);
+void print_boat_fleet(const Boat boat);
